4b90ee0aafe265b508b8a3dceb7d2024c80ccf95
JW0203 <widwoo0203@gmail.com>
2024-04-18 13:56:53 +0900

    fix: remove the duplicated metadata

--- official-document/src/app.module.ts
+++ official-document/src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
-import {CatsModule} from "./cats/cats.module";
+// import {CatsModule} from "./cats/cats.module";
 
 @Module({
-  imports: [CatsModule],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
