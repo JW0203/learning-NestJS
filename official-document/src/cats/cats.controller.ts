00c9c975fffb53c5c42e46d7f9de01f6998be4a3
JW0203 <widwoo0203@gmail.com>
2024-04-18 13:17:54 +0900

    fix : change codes

--- official-document/src/cats/cats.controller.ts
+++ official-document/src/cats/cats.controller.ts
@@ -1,9 +1,24 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Get, Param, ParseIntPipe, Post} from '@nestjs/common';
+import {CatsService} from "./cats.service";
+import {CreateCatDto} from "./dto/create-cat.dto";
+import {Cat} from "./interfaces/cst.interface";
 
 @Controller('cats')
 export class CatsController {
+    constructor(private catsService: CatsService) {}
+
+    @Post()
+    async create(@Body() createCatDto: CreateCatDto){
+        this.catsService.create(createCatDto);
+    }
+
     @Get()
-    findAll():string{
-        return "This action returns all cats"
+    async findAll():Promise<Cat[]>{
+        console.log("This action returns all cats")
+        return this.catsService.findAll()
+    }
+    @Get(':id')
+    findOne(@Param('id', new ParseIntPipe()) id:number){
+
     }
 }
--- official-document/src/cats/cats.service.ts
+++ official-document/src/cats/cats.service.ts
@@ -10,6 +10,6 @@ export class CatsService {
     }
 
     findAll(): Cat[]{
-        return this.cats;
+        return this.cats
     }
 }
